4cca81b2f5ca3d674305930927cc6bd82ae65918
Guilherme Sanches <guitdsanches@gmail.com>
2023-03-29 18:54:20 -0300

    Refactoring the input component and adding the new function of selecting a team's color

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,5 +1,5 @@
 import './Formulario.css';
-import CampoTexto from '../CampoTexto';
+import Campo from '../Campo';
 import ListaSuspensa from '../ListaSuspensa';
 import Botao from '../Botao';
 import {useState} from 'react';
@@ -32,21 +32,24 @@ const Formulario = (props) => {
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto 
+                <Campo
+                    type='text' 
                     obrigatorio={true}
                     label="Nome" 
                     placeholder="Digite seu nome"
                     valor={nome}
                     aoAlterado={valor => setNome(valor)}
                 />
-                <CampoTexto 
+                <Campo
+                    type='text' 
                     obrigatorio={true} 
                     label="Cargo" 
                     placeholder="Digite seu cargo" 
                     valor={cargo}
                     aoAlterado={valor => setCargo(valor)}
                 />
-                <CampoTexto 
+                <Campo
+                    type='text' 
                     label="Imagem" 
                     placeholder="Digite o endereço da imagem" 
                     valor={imagem}
@@ -68,14 +71,16 @@ const Formulario = (props) => {
                 props.cadastrarTime({ nome: nomeTime, cor: corTime})
             }}>
                 <h2>Preencha os dados para criar um novo time</h2>
-                <CampoTexto 
+                <Campo
+                    type='text' 
                     obrigatorio
                     label="Nome" 
                     placeholder="Digite o nome do time"
                     valor={nomeTime}
                     aoAlterado={valor => setNomeTime(valor)}
                 />
-                <CampoTexto 
+                <Campo
+                    type='color' 
                     obrigatorio 
                     label="Cargo" 
                     placeholder="Digite a cor do time" 
