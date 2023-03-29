98e5e82f03ece267b0151cbb63f60c0ee35a73d8
Guilherme Sanches <guitdsanches@gmail.com>
2023-03-29 10:04:20 -0300

    adding new color change feature

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -4,6 +4,7 @@ import './Time.css';
 const Time = (props) => {
     return (
         props.colaboradores.length > 0 ? <section className="time" style={{ backgroundColor: props.corSecundaria}}> {/* se a lista de colaboradores de um determinado time estiver vazia, não exibe nada */}
+            <input value={props.corSecundaria} type='color' className='input-cor' />
             <h3 style={{ borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map(colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} aoDeletar={props.aoDeletar}/>)}    
